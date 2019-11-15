b5e525baeb061e687d700696ccee599f56557416
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2019-11-14 18:33:06 -0800

    Add Vim plugins to dot.sh

--- dot.sh
+++ dot.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# Update the repo
-git pull
-
+# Install symlinks
 printf "%b\n" "\e[36;1mBacking up existing dotfiles...\e[0m"
-if [ ! -L ~/.gitconfig ]
-then
+if [ ! -L ~/.gitconfig ]; then
     mv --interactive --verbose ~/.gitconfig ~/.gitconfig.bak
 fi
 
@@ -23,6 +20,15 @@ printf "%b\n" "\e[36;1mCreating symlinks...\e[0m"
 ln --force --no-dereference --symbolic --verbose $PWD/.gitconfig ~/.gitconfig
 ln --force --no-dereference --symbolic --verbose $PWD/.vimrc ~/.vimrc
 ln --force --no-dereference --symbolic --verbose $PWD/.tmux.conf ~/.tmux.conf
+
+# Install vim plugins
+printf "%b\n" "\e[36;1mInstalling Vim plugins...\e[0m"
+if [ ! -d ~/.vim/autoload ]; then
+    curl --create-dirs -o ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    vim -c "PlugInstall | quit | quit"
+else
+    vim -c "PlugUpgrade | PlugUpdate | quit | quit"
+fi
 
 printf "%b\n" "\e[33;1mUse \e[35;1mrm -ir ~/.*.bak\e[33;1m to remove backups.\e[0m"
 printf "%b\n" "\e[32mDotfile installation completed.\e[0m"
