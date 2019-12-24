89777bf405b8faa4ca4c2509b7d2bec60f228038
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2019-12-24 10:11:06 -0800

    Add UI improvements

--- install.sh
+++ install.sh
@@ -4,28 +4,31 @@
 git pull
 
 # Install symlinks
-printf "%b\n" "\e[36;1mBacking up existing dotfiles...\e[0m"
+backups=0
 if [ ! -L ~/.gitconfig ]; then
     mv --interactive --verbose ~/.gitconfig ~/.gitconfig.bak
+    backups=1
 fi
 
 if [ ! -L ~/.vimrc ]; then
     mv --interactive --verbose ~/.vimrc ~/.vimrc.bak
+    backups=1
 fi
 
 if [ ! -L ~/.tmux.conf ]; then
     mv --interactive --verbose ~/.tmux.conf ~/.tmux.conf.bak
+    backups=1
 fi
 
 if [ ! -L ~/.bashrc ]; then
     mv --interactive --verbose ~/.bashrc ~/.bashrc.bak
+    backups=1
 fi
 
-if [ ! -L !/.bash_profile ]; then
+if [ ! -L ~/.bash_profile ]; then
     mv --interactive --verbose ~/.bash_profile ~/.bash_profile.bak
+    backups=1
 fi
-
-printf "%b\n" "\e[36;1mCreating symlinks...\e[0m"
 
 ln --force --no-dereference --symbolic --verbose $PWD/.gitconfig ~/.gitconfig
 ln --force --no-dereference --symbolic --verbose $PWD/.vimrc ~/.vimrc
@@ -35,7 +38,6 @@ ln --force --no-dereference --symbolic --verbose $PWD/.bash_profile ~/.bash_prof
 ln --force --no-dereference --symbolic --verbose $PWD/shell ~/.shell
 
 # Install vim plugins
-printf "%b\n" "\e[36;1mInstalling Vim plugins...\e[0m"
 if [ ! -d ~/.vim/autoload ]; then
     curl --create-dirs -o ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     vim -c "PlugInstall | quit | quit"
@@ -43,5 +45,8 @@ else
     vim -c "PlugUpgrade | PlugUpdate | quit | quit"
 fi
 
-printf "%b\n" "\e[32mUse \e[35;1mrm -ir ~/.*.bak\e[32m to remove backups.\e[0m\n"
+if [ "$backups" -eq 1 ]; then
+    printf "%b\n" "\e[32mUse \e[35;1mrm -ir ~/.*.bak\e[0;32m to remove backups.\e[0m"
+fi
+
 printf "%b\n" "\e[32mDotfile installation completed. Restart the shell or type \e[35;1m. ~/.bashrc\e[0;32m to load shell environment.\e[0m"
