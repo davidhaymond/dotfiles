3ac277aff62f0b8036393579ff20becf55bbf613
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-01-18 21:16:39 -0800

    Add dotfiles and SSH functions to PS profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,3 +17,29 @@ filter ConvertFrom-Base64String {
 
     [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($InputString))
 }
+
+function update-dotfiles {
+    Push-Location -Path ~\.dotfiles
+    & .\install.ps1
+    Pop-Location
+}
+
+function new-key {
+    $keyPath = Resolve-Path -Path ~\.ssh\id_ed25519
+    ssh-keygen -t ed25519 -a 64 -C haymondtechnologies@gmail.com -f "$keyPath"
+    Write-Output "%b\n" "\e[36;1mPublic key:\e[0m"
+    Get-Content -Path "$keyPath.pub"
+}
+
+function clone {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory = $true,
+            Position = 0
+        )]
+        [string] $Repo
+    )
+
+    git clone git@github.com:$Repo.git
+}
