81871ef14c86556b0f2f0eaf5f51cf6ae2e345fb
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-01-26 12:06:07 -0800

    Update bootstrapped Linux packages

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -33,8 +33,8 @@ echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a $N
 
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install curl git nodejs tmux vim xdg-utils -y   # xdg-utils is needed for vim-instant-markdown plugin
-sudo npm install -g npm instant-markdown-d browser-sync
+sudo apt-get install git nodejs tmux vim xdg-utils -y   # xdg-utils is needed for vim-instant-markdown plugin
+sudo npm install -g npm instant-markdown-d browser-sync netlify-cli
 
 
 # Configure system Git settings
