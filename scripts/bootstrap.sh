5f37fd6243fb6d9b8c57a945d87b4d714682c2ed
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-11 19:13:36 -0800

    Reduce bootstrap.sh terminal output

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ fi
 # Update package cache
 # --------------------
 
-sudo apt-get update -y
+sudo apt-get update -qy
 
 
 # Add Nodesource repository
@@ -17,24 +17,24 @@ sudo apt-get update -y
 
 if [ ! -f /etc/apt/trusted.gpg.d/nodesource.gpg ]; then
     curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | \
-	    sudo apt-key --keyring /etc/apt/trusted.gpg.d/nodesource.gpg add -
+        sudo apt-key --keyring /etc/apt/trusted.gpg.d/nodesource.gpg add - &> /dev/null
 fi
 
 NODESOURCE_PATH="/etc/apt/sources.list.d/nodesource.list"
-sudo apt-get install lsb-release -y
+sudo apt-get install lsb-release -qy
 VERSION="node_12.x"
 DISTRO="$(lsb_release -cs)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee $NODESOURCE_PATH
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a $NODESOURCE_PATH
+echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee $NODESOURCE_PATH > /dev/null
+echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a $NODESOURCE_PATH /dev/null
 
 
 # Install packages
 # ----------------
 
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install git nodejs tmux vim xdg-utils -y   # xdg-utils is needed for vim-instant-markdown plugin
-sudo npm install -g npm instant-markdown-d browser-sync netlify-cli
+sudo apt-get update -qy
+sudo apt-get dist-upgrade -qy
+sudo apt-get install git nodejs tmux vim xdg-utils -qy   # xdg-utils is needed for vim-instant-markdown plugin
+sudo npm install -gq npm instant-markdown-d browser-sync netlify-cli
 
 
 # Configure system Git settings
@@ -46,16 +46,16 @@ sudo git config --system core.autocrlf input
 # -----------------------------
 
 if [ ! -d ~/.dotfiles ]; then
-    pushd ~
+    pushd ~ > /dev/null
 
     # Clone with HTTPS so no password is required,
     # and then change the remote push URL to SSH
-    git clone https://github.com/davidhaymond/dotfiles.git .dotfiles
+    git clone -q https://github.com/davidhaymond/dotfiles.git .dotfiles
     cd .dotfiles
     git remote set-url --push origin git@github.com:davidhaymond/dotfiles.git
 else
-    pushd ~/.dotfiles
+    pushd ~/.dotfiles > /dev/null
 fi
 
 bash scripts/install.sh
-popd
+popd > /dev/null
