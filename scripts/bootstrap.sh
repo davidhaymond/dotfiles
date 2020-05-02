aaeb701c1ead9895d0765fcd8fb884954987360b
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-05-02 11:00:18 -0700

    Remove unrelated bootstrap.sh code
    To narrow the focus of bootstrap.sh and improve compatibility with
    multiple distros, I've removed software installation and anything that
    isn't directly related to installing the dotfiles.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,49 +11,12 @@ if [ -L ~/.gitconfig ]; then
     rm -f ~/.gitconfig
 fi
 
-
-# Update package cache
-# --------------------
-
-sudo apt-get update -qy
-
-
-# Add Nodesource repository
-# -------------------------
-
-if [ ! -f /etc/apt/trusted.gpg.d/nodesource.gpg ]; then
-    curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | \
-        sudo apt-key --keyring /etc/apt/trusted.gpg.d/nodesource.gpg add - &> /dev/null
-fi
-
-NODESOURCE_PATH="/etc/apt/sources.list.d/nodesource.list"
-sudo apt-get install lsb-release -qy
-VERSION="node_12.x"
-DISTRO="$(lsb_release -cs)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee $NODESOURCE_PATH > /dev/null
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a $NODESOURCE_PATH > /dev/null
-
-
-# Install packages
-# ----------------
-
-sudo apt-get update -qy
-sudo apt-get dist-upgrade -qy
-sudo apt-get install git nodejs tmux vim xdg-utils curl apt-transport-https -qy   # xdg-utils is needed for vim-instant-markdown plugin
-sudo npm install -gq npm instant-markdown-d browser-sync netlify-cli
-
-
 # Configure system Git settings
-# -----------------------------
 sudo git config --system core.autocrlf input
 
-
 # Clone or update dotfiles repo
-# -----------------------------
-
 if [ ! -d ~/.dotfiles ]; then
     pushd ~ > /dev/null
-
     # Clone with HTTPS so no password is required,
     # and then change the remote push URL to SSH
     git clone -q https://github.com/davidhaymond/dotfiles.git .dotfiles
