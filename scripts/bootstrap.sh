f491bbabacb9dc0992296f5c667193d55b254ec5
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-12-15 15:36:54 -0800

    Replace tilde with $HOME in bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,30 +2,30 @@
 
 # Delete any existing .gitconfig symlink
 # to prevent Git errors caused by broken links
-if [ -L ~/.gitconfig ]; then
-    rm -f ~/.gitconfig
+if [ -L $HOME/.gitconfig ]; then
+    rm -f $HOME/.gitconfig
 fi
 
 # Clone or update dotfiles repo
-if [ ! -d ~/.dotfiles ]; then
-    pushd ~ > /dev/null
+if [ ! -d $HOME/.dotfiles ]; then
+    pushd $HOME > /dev/null
     # Clone with HTTPS so no password is required,
     # and then change the remote push URL to SSH
     git clone -q https://github.com/davidhaymond/dotfiles.git .dotfiles
     cd .dotfiles
     git remote set-url --push origin git@github.com:davidhaymond/dotfiles.git
 else
-    pushd ~/.dotfiles > /dev/null
+    pushd $HOME/.dotfiles > /dev/null
 fi
 
 bash scripts/install.sh
 
-cat << 'EOF' >> ~/.bashrc
+popd > /dev/null
+
+cat << 'EOF' >> $HOME/.bashrc
 
 # Load custom shell scripts from dotfiles
-for f in ~/.dotfiles/shell/*; do
+for f in $HOME/.dotfiles/shell/*; do
     source $f
 done
 EOF
-
-popd > /dev/null
